7b3257e43b0c5173d3e94ccba76ee8846a071a00
matheussalencar <matheus.hsantos@mercadolivre.com>
2022-01-27 11:16:23 -0300

    inicio do requisito 1

---
+++ src/main/java/com/w4/projetoIntegrador/entities/Section.java
@@ -0,0 +1,10 @@
+package com.w4.projetoIntegrador.entities;
+
+import com.w4.projetoIntegrador.enums.ProductTypes;
+
+public class Section {
+    private Long id;
+    private ProductTypes type;
+    private float totalSpace;
+    private Warehouse warehouse;
+}
