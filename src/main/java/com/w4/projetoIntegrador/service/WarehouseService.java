029a56bc5e8136a77f5121ea43acb1be89f366a3
luccorrea <lucas.correa@mercadolivre.com>
2022-02-03 16:50:19 -0300

    requisito 4

--- src/main/java/com/w4/projetoIntegrador/service/WarehouseService.java
+++ src/main/java/com/w4/projetoIntegrador/service/WarehouseService.java
@@ -1,10 +1,15 @@
 package com.w4.projetoIntegrador.service;
 
+import com.w4.projetoIntegrador.dtos.ProductsByWarehouseDto;
+import com.w4.projetoIntegrador.dtos.WarehouseStockDto;
 import com.w4.projetoIntegrador.entities.Warehouse;
 import com.w4.projetoIntegrador.exceptions.NotFoundException;
 import com.w4.projetoIntegrador.repository.WarehouseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class WarehouseService {
@@ -22,6 +27,19 @@ public class WarehouseService {
 
     public Warehouse save(Warehouse wh) {
         return warehouseRepository.save(wh);
+    }
+
+    public ProductsByWarehouseDto getWarehouseStock(Long id){
+       List<WarehouseRepository.ProductWarehouse> list = warehouseRepository.getStockByWarehouse(id);
+       ProductsByWarehouseDto pto = ProductsByWarehouseDto.builder().productId(id)
+               .build();
+       List<WarehouseStockDto> wd = new ArrayList<>();
+        for (WarehouseRepository.ProductWarehouse item:list){
+            WarehouseStockDto ws = WarehouseStockDto.builder().warehosecode(item.getWarehouse()).totalquantity(item.getStock()).build();
+            wd.add(ws);
+        }
+        pto.setWarehouses(wd);
+        return pto;
     }
 }
 
